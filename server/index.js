ddb198d79ad1c445c350099bf9029281ef608ac1
Sherzod Yoqubov <yoqubovsherzod1997@gmail.com>
2023-12-10 09:33:57 +0500

    api

--- server/index.js
+++ server/index.js
@@ -4,6 +4,18 @@ const token = '6886269433:AAHbu7y2mrf1IjfgQ4Z_9nEYNkW5jjIlebM'
 const bot = new TelegramBot(token, { polling: true })
 
 const bootstrap = () => {
+
+  bot.setMyCommands([
+    {
+      command: '/start',
+      description: 'Xush Kelibsiz'
+    },
+    {
+      command: '/cources',
+      description: 'Barcha productlarni ko\'rish'
+    }
+  ])
+
   bot.on('message', async (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
@@ -25,6 +37,48 @@ const bootstrap = () => {
         }
       }
       )
+    }
+
+    if(text === '/cources'){
+      await bot.sendMessage(
+        chatId,
+        'All courses',
+        {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: 'Kurslarni ko\'rish',
+                  web_app: {
+                    url: 'https://telegram-web-bot-client.vercel.app',
+                  }
+                }
+              ]
+            ]
+          }
+        }
+      )
+    }
+
+    if(msg.web_app_data?.data){
+      try {
+        const data = JSON.parse(msg.web_app_data?.data)
+        await bot.sendMessage(chatId, "Siz mahsulotni muvovaqqiyatli sotib oldingiz")
+        for(item of data){
+          await bot.sendPhoto(chatId, item.Image)
+          await bot.sendMessage(
+            chatId,
+            `${item.title} - ${item.quantity}x`
+          )
+        }
+
+        await bot.sendMessage(
+          chatId,
+          `Umumiy narx: ${data.reduce((a,c) => a + c.price * c.quantity,0).toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`
+        )
+      } catch (error) {
+        console.log(error)
+      }
     }
   })
 }
